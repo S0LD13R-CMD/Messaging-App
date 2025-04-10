b6be5c477dd8936afc017fa4b7f92904f3738d4d
Abdul Rauf <sh221may@gmail.com>
2025-04-10 09:06:05 +0100

    Refactor WebSocket and Security configurations to enforce non-null parameters. Update WebSocketHandshakeInterceptor to handle nullable exceptions and improve user determination logic.

--- messaging.app/src/main/java/com/qmul/messaging/app/config/WebSocketHandshakeInterceptor.java
+++ messaging.app/src/main/java/com/qmul/messaging/app/config/WebSocketHandshakeInterceptor.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
@@ -13,10 +15,10 @@ import java.util.Map;
 @Component
 public class WebSocketHandshakeInterceptor implements HandshakeInterceptor {
     @Override
-    public boolean beforeHandshake(ServerHttpRequest request,
-                                   ServerHttpResponse response,
-                                   WebSocketHandler wsHandler,
-                                   Map<String, Object> attributes) {
+    public boolean beforeHandshake(@NonNull ServerHttpRequest request,
+                                   @NonNull ServerHttpResponse response,
+                                   @NonNull WebSocketHandler wsHandler,
+                                   @NonNull Map<String, Object> attributes) {
         if (request instanceof ServletServerHttpRequest servletRequest) {
             HttpSession session = servletRequest.getServletRequest().getSession(false);
             if (session != null) {
@@ -30,6 +32,6 @@ public class WebSocketHandshakeInterceptor implements HandshakeInterceptor {
     }
 
     @Override
-    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response,
-                               WebSocketHandler wsHandler, Exception exception) {}
+    public void afterHandshake(@NonNull ServerHttpRequest request, @NonNull ServerHttpResponse response,
+                               @NonNull WebSocketHandler wsHandler, @Nullable Exception exception) {}
 }
