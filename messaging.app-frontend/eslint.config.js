99ce125febb079805a05a28cee3e20300601cc81
Abdul Rauf <sh221may@gmail.com>
2025-04-17 17:55:21 +0100

    Update build script in package.json to simplify the build process by removing TypeScript compilation step, streamlining the frontend build workflow.

---
+++ messaging.app-frontend/eslint.config.js
@@ -0,0 +1,42 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginReactRefresh from "eslint-plugin-react-refresh";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ...pluginReactConfig,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      ...pluginReactConfig.rules,
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-no-target-blank": "off",
+    }
+  },
+  {
+    plugins: { "react-hooks": pluginReactHooks },
+    rules: pluginReactHooks.configs.recommended.rules,
+  },
+  {
+    plugins: { "react-refresh": pluginReactRefresh },
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+  {
+    ignores: ["dist/", "node_modules/", "vite.config.ts", "postcss.config.js", "tailwind.config.js", "eslint.config.js"]
+  }
+]; 
