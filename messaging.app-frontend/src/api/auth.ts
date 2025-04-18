33f1cf49ccef87d2e3a9bba14212988e0d8b260a
Abdul Rauf <sh221may@gmail.com>
2025-04-18 16:37:19 +0100

    Update API base URL in auth.ts to use localhost for local development, ensuring proper connection during testing.

--- messaging.app-frontend/src/api/auth.ts
+++ messaging.app-frontend/src/api/auth.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api.yappatron.org', // We set this to our backend - Change for Abdul when necessary.
+    baseURL: 'http://localhost:8080', // We set this to our backend - Change for Abdul when necessary.
     withCredentials: true, // Include cookies for credentials on requests.
     headers: {
         'Content-Type': 'application/json',
